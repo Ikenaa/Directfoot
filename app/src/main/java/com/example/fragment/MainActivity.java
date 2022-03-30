0012b02666bb024b76d4e22721f60b97701abea0
Ikena <rayyantounsy@gmail.com>
2022-03-31 00:02:33 +0200

    splasshhhh suite

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,9 +18,10 @@ import com.example.BackEnd.Championship;
 
 public class MainActivity extends AppCompatActivity{
 
-    private FragmentHome fragment1;
+    private Fragment fragment1;
     private Button AccueilFragmentBtn, ClassementFragmentBtn, SearchFragmentBtn, DirectFragmentButton;
 
+   // private SwipeListener swipeListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +30,11 @@ public class MainActivity extends AppCompatActivity{
 
         setContentView(R.layout.activity_main);
 
-        fragment1 = new FragmentHome(mainActivity);
-        fragment1.setFragmentActivity(this);
+
+
+
+        fragment1 = new FragmentDirect(mainActivity);
+
 
         replaceFragment(fragment1);
 
@@ -42,7 +51,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View view) {
                 fragment1 = new FragmentHome(mainActivity);
-                fragment1.setFragmentActivity(mainActivity);
+
 
 
                 replaceFragment(fragment1);
@@ -56,6 +65,7 @@ public class MainActivity extends AppCompatActivity{
 
 
                 replaceFragment(new FragmentTableHome(mainActivity));
+
 
             }
         });*/
@@ -80,15 +90,6 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-     /*   FavorisFragmentBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-                replaceFragment(new FragmentFavoris());
-
-            }
-        });*/
 
 
     }
@@ -105,5 +106,45 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
+    public void removeFragment(Fragment fragment) {
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.remove(fragment);
+
+        fragmentTransaction.commit();
+
+    }
+
+ /*   @Override
+    public void onBackPressed() {
+
+       removeFragment(fragment1);
+    }
+
+
+    private class SwipeListener implements View.OnTouchListener {
+
+        GestureDetector gestureDetector;
+
+        SwipeListener(View view){
+            int threshold = 100;
+            int velocity_threshold = 100;
+
+            GestureDetector.SimpleOnGestureListener listener =
+                    new GestureDetector.SimpleOnGestureListener(){
+                        @Override
+                        public boolean onLeft(MotionEvent e){
+
+                        }
+                    }
+        }
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            return false;
+        }
+    }*/
 }
